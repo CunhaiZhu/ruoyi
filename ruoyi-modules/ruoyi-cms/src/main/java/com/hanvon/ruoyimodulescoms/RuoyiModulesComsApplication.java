65e45c1156ea4f26c0c427bb20c402a5c538800f
Hardy <bSb!hK9g6Q#9YEka>
2021-06-29 18:54:17 +0800

    pack

---
+++ ruoyi-modules/ruoyi-cms/src/main/java/com/hanvon/ruoyimodulescoms/RuoyiModulesComsApplication.java
@@ -0,0 +1,13 @@
+package com.hanvon.ruoyimodulescoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuoyiModulesComsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuoyiModulesComsApplication.class, args);
+	}
+
+}
---
+++ ruoyi-modules/ruoyi-cms/src/test/java/com/hanvon/ruoyimodulescoms/RuoyiModulesComsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanvon.ruoyimodulescoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuoyiModulesComsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
